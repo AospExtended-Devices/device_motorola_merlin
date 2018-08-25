1f999092782cd45eb26d66450ec963f9505a385f
innfinite4evr <innfinite4evr@gmail.com>
2018-08-25 20:16:55 +0530

    Update init_merlin.cpp

--- init/init_merlin.cpp
+++ init/init_merlin.cpp
@@ -86,8 +86,8 @@ void vendor_load_properties()
     sprintf(fingerprint, "motorola/merlin_%s/merlin:6.0.1/MPD24.107-56/30:user/release-keys", customerid);
 
     property_override_dual("ro.product.device","ro.vendor.product.device", device);
-    property_override("ro.build.product", device);
-    property_override("ro.build.description", description);
+    property_override_dual("ro.build.product", "ro.vendor.build.product", device);
+    property_override_dual("ro.build.description", "ro.vendor.build.description", description);
     property_override_dual("ro.build.fingerprint","ro.vendor.build.fingerprint", fingerprint);
     property_set("ro.mot.build.customerid", customerid);
 }
